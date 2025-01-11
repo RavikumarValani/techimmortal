a7201f82a7f5da0130f646bf1f67cbab06c06fb7
Ravi Valani <valaniravi11111@gmail.com>
2025-01-11 23:00:22 +0530

    Front slider changes

--- Front/app/components/LogoSlider.js
+++ Front/app/components/LogoSlider.js
@@ -4,110 +4,110 @@ const LogoSlider = () => {
   return (
     <section className="py-12 lg:py-24">
       <div className="">
-        <div className="trusted_content mb-4" data-aos="fade-up">
-          <p className="md:text-5xl text-3xl font-bold text-gray-300 text-center mb-4">
-            Technologies in <span className="text-[#CEFF05]">Action</span>
-          </p>
-          <p className="md:text-xl text-lg font-medium text-gray-300 text-center mb-12">
-            <span className="text-[#CEFF05]">
-              Looking for the best results?
-            </span>{" "}
-            We use cutting-edge technology to make it happen!
-          </p>
+        <div className="mb-4">
+          <p className="md:text-5xl text-3xl font-bold text-gray-300 text-center mb-4">Technologies in action </p>
+          <p className="md:text-xl text-lg font-medium text-gray-300 text-center mb-16">Why compromise? When we have the best technologies for the best outcome </p>
         </div>
 
         <div className="slider">
           <div className="slide-track flex">
             <div className="slide ml-8">
-              <img loading="lazy" src="/react-logo.svg" alt="" title="" />
+              <img loading="lazy" src="/react-logo.svg" alt="" title="react" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/next-js.svg" alt="" title="" />
+              <img loading="lazy" src="/next-js.svg" alt="" title="next js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/three-js.svg" alt="" title="" />
+              <img loading="lazy" src="/three-js.svg" alt="" title="three js" />
             </div>
             <div className="slide ml-8">
-              <img
-                loading="lazy"
-                className="node-js"
-                src="/node-js.svg"
-                alt=""
-                title=""
-              />
+              <img loading="lazy" src="/express-js.svg" alt="" title="express js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/express-js.svg" alt="" title="" />
+              <img loading="lazy" src="/golang.svg" alt="" title="golang" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/golang.svg" alt="" title="" />
+              <img loading="lazy" src="/mongo-db.svg" alt="" title="mong0 db" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/mongo-db.svg" alt="" title="" />
+              <img loading="lazy" src="/postgresql.svg" alt="" title="postgresql" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/postgresql.svg" alt="" title="" />
+              <img loading="lazy" src="/node-js.svg" alt="" title="node js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/react-logo.svg" alt="" title="" />
+              <img loading="lazy" src="/react-logo.svg" alt="" title="react" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/next-js.svg" alt="" title="" />
+              <img loading="lazy" src="/next-js.svg" alt="" title="next js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/three-js.svg" alt="" title="" />
+              <img loading="lazy" src="/three-js.svg" alt="" title="three js" />
             </div>
             <div className="slide ml-8">
-              <img
-                loading="lazy"
-                className="node-js"
-                src="/node-js.svg"
-                alt=""
-                title=""
-              />
+              <img loading="lazy" src="/express-js.svg" alt="" title="express js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/express-js.svg" alt="" title="" />
+              <img loading="lazy" src="/golang.svg" alt="" title="golang" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/golang.svg" alt="" title="" />
+              <img loading="lazy" src="/mongo-db.svg" alt="" title="mong0 db" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/mongo-db.svg" alt="" title="" />
+              <img loading="lazy" src="/postgresql.svg" alt="" title="postgresql" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/postgresql.svg" alt="" title="" />
+              <img loading="lazy" src="/node-js.svg" alt="" title="node js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/react-logo.svg" alt="" title="" />
+              <img loading="lazy" src="/react-logo.svg" alt="" title="react" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/next-js.svg" alt="" title="" />
+              <img loading="lazy" src="/next-js.svg" alt="" title="next js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/three-js.svg" alt="" title="" />
+              <img loading="lazy" src="/three-js.svg" alt="" title="three js" />
             </div>
             <div className="slide ml-8">
-              <img
-                loading="lazy"
-                className="node-js"
-                src="/node-js.svg"
-                alt=""
-                title=""
-              />
+              <img loading="lazy" src="/express-js.svg" alt="" title="express js" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/express-js.svg" alt="" title="" />
+              <img loading="lazy" src="/golang.svg" alt="" title="golang" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/golang.svg" alt="" title="" />
+              <img loading="lazy" src="/mongo-db.svg" alt="" title="mong0 db" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/mongo-db.svg" alt="" title="" />
+              <img loading="lazy" src="/postgresql.svg" alt="" title="postgresql" />
             </div>
             <div className="slide ml-8">
-              <img loading="lazy" src="/postgresql.svg" alt="" title="" />
+              <img loading="lazy" src="/node-js.svg" alt="" title="node js" />
             </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/react-logo.svg" alt="" title="react" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/next-js.svg" alt="" title="next js" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/three-js.svg" alt="" title="three js" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/express-js.svg" alt="" title="express js" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/golang.svg" alt="" title="golang" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/mongo-db.svg" alt="" title="mong0 db" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/postgresql.svg" alt="" title="postgresql" />
+            </div>
+            <div className="slide ml-8">
+              <img loading="lazy" src="/node-js.svg" alt="" title="node js" />
+            </div>
+
           </div>
         </div>
       </div>
