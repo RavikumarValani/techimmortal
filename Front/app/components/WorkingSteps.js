a641477f667c69ee7967c6e7f63c3b0f4d148062
mihir <mihirshah1206@gmail.com>
2025-01-12 12:45:49 +0530

    Front changes

---
+++ Front/app/components/WorkingSteps.js
@@ -0,0 +1,181 @@
+import React from "react";
+
+const WorkingSteps = () => {
+  return (
+    <section className="py-12 lg:py-24">
+      <div className="">
+        <p className="md:text-5xl text-3xl font-bold text-gray-300 text-center mb-4">
+          Work <span className="text-[#CEFF05]">smarter,</span> achieve
+          <span className="text-[#CEFF05]"> faster</span>
+        </p>
+        <p className="md:text-xl text-lg font-medium text-gray-300 text-center mb-16">
+          With a clear roadmap in hand, you're already halfway to success.
+        </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-lightbulb text-[#CEFF05]"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    1
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  PROJECT IDEA
+                </h2>
+                <p className="text-gray-300">We grasp your vision.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-compass"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    2
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  DISCOVERY
+                </h2>
+                <p className="text-gray-300">
+                  We dive deep into project research.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-users-cog"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    3
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  TEAM SELECTION
+                </h2>
+                <p className="text-gray-300">We handpick the ideal team.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-pencil-ruler"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    4
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  PRODUCT DESIGN
+                </h2>
+                <p className="text-gray-300">
+                  We begin designing and refining the product.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-road"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    5
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  ROADMAP & SPRINT PLAN
+                </h2>
+                <p className="text-gray-300">
+                  We create a roadmap and plan sprints for the project.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-code"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    6
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  DEVELOPMENT
+                </h2>
+                <p className="text-gray-300">
+                  Development begins according to the project type.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-flask"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    7
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  TESTING
+                </h2>
+                <p className="text-gray-300">We test and refine the project.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative">
+            <div className="bg-slate-800 rounded-lg p-6 h-full flex flex-col justify-between border border-slate-700 transition-all duration-300 hover:border-[#CEFF05] group">
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <span className="text-3xl md:text-4xl font-bold text-[#CEFF05]">
+                    <i className="fas fa-box"></i>
+                  </span>
+                  <div className="p-2 bg-slate-700/50 rounded-lg font-bold text-[#CEFF05]">
+                    8
+                  </div>
+                </div>
+                <h2 className="text-base md:text-lg font-semibold text-white mb-1">
+                  DELIVERY
+                </h2>
+                <p className="text-gray-300">Project delivery is complete.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkingSteps;
